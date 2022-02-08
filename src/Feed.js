eebcde47bd4cfe9234cadb345834712bd5c5200c
aearavindh <aearavindh@gmail.com>
2022-02-09 00:15:42 +0530

    [Redux, FlipMove, Authentication completed]

--- src/Feed.js
+++ src/Feed.js
@@ -15,6 +15,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import { useSelector } from "react-redux";
 import { selectUser } from "./features/userSlice";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [input, setInput] = useState("");
@@ -77,15 +78,17 @@ function Feed() {
         </div>
       </div>
 
-      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
-        <Post
-          key={id}
-          name={name}
-          description={description}
-          message={message}
-          photoUrl={photoUrl}
-        />
-      ))}
+      <FlipMove>
+        {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+          <Post
+            key={id}
+            name={name}
+            description={description}
+            message={message}
+            photoUrl={photoUrl}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/Widgets.js
+++ src/Widgets.js
@@ -1,10 +1,31 @@
+import { FiberManualRecord, Info } from "@mui/icons-material";
 import React from "react";
 import "./Widgets.css";
 
 function Widgets() {
-  return <div className="widgets">
-      <img src="https://media-exp1.licdn.com/dms/image/C5616AQGwy3leKcZjuw/profile-displaybackgroundimage-shrink_350_1400/0/1626248661594?e=1649894400&v=beta&t=9pFhTkIwno3r0RJa8hRLPlLa1ecTemVW_R19srAvcvU" alt="" />
-  </div>;
+  const newsArticle = (heading, subtitle) => (
+    <div className="widgets__article">
+      <div className="widgets__articleLeft">
+        <FiberManualRecord />
+      </div>
+      <div className="widgets__articleRight">
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="widgets">
+      <div className="widgets__header">
+        <h2>LinkedIn News</h2>
+        <Info />
+      </div>
+      {newsArticle("2022 - the year of revenge hiring", "7,062 readers")}
+      {newsArticle("COVID 19 - Outbreak", "5,054 readers")}
+      {newsArticle("TCS, Infosys, Wipro - mass hiring", "2,464 readers")}
+    </div>
+  );
 }
 
 export default Widgets;
