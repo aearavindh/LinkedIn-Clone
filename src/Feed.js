84ea06fb03a2a9397776c675b97e409a1eeecad9
aearavindh <aearavindh@gmail.com>
2022-02-08 20:18:03 +0530

    [Redux, Firebase, Login page]

--- src/Feed.js
+++ src/Feed.js
@@ -6,15 +6,44 @@ import {
   Subscriptions,
 } from "@mui/icons-material";
 import React, { useState } from "react";
+import { useEffect } from "react";
 import "./Feed.css";
+import { db } from "./firebase";
 import InputOption from "./InputOption";
 import Post from "./Post";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
 
 function Feed() {
+  const [input, setInput] = useState("");
   const [posts, setPosts] = useState([]);
 
-  const sendPost = e => {
-      e.preventDefault();
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      name: "AE Aravindh",
+      description: "Software Engineer",
+      message: input,
+      photoUrl:
+        "https://media-exp1.licdn.com/dms/image/C5603AQEOpwdWjTRq8Q/profile-displayphoto-shrink_400_400/0/1615721046640?e=1649894400&v=beta&t=5xFFzddYj9H4JUfzXQ-34H-6sWFlsyO64on_lIqF2II",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
   };
 
   return (
@@ -23,7 +52,11 @@ function Feed() {
         <div className="feed__input">
           <Create />
           <form>
-            <input type="text" />
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
             <button onClick={sendPost} type="submit">
               Send
             </button>
@@ -41,16 +74,15 @@ function Feed() {
         </div>
       </div>
 
-      {posts.map((post) => (
-        <Post />
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
       ))}
-
-      <Post
-        name="AE Aravindh"
-        description="Software Engineer"
-        message="Hey there! It's me"
-        photoUrl="https://media-exp1.licdn.com/dms/image/C5603AQEOpwdWjTRq8Q/profile-displayphoto-shrink_400_400/0/1615721046640?e=1649894400&v=beta&t=5xFFzddYj9H4JUfzXQ-34H-6sWFlsyO64on_lIqF2II"
-      />
     </div>
   );
 }
