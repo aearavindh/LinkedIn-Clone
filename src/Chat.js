b7e58a1a287ec5c222e598e2cde4ffd3a996dd38
aearavindh <aearavindh@gmail.com>
2022-02-10 22:50:36 +0530

    [animation added to chat]

--- src/Chat.js
+++ src/Chat.js
@@ -48,52 +48,53 @@ function Chat() {
           </IconButton>
         </div>
       </div>
-      {active && (
-        <div className="chat__body">
-          <div className="chat__search">
-            <Search />
-            <input type="text" placeholder="Search messages" />
-            <IconButton disableRipple>
-              <MenuIcon />
-            </IconButton>
-          </div>
-          <div className="chat_rooms">
-            <RoomInfo
-              profilePic="https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg"
-              name="Elon Musk"
-              isLastMessageSentByMe
-              lastMessage="Hi Musk! How are you?"
-              timestamp="8:11 PM"
-            />
-            <RoomInfo
-              profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgvp8NhpQNyMKvqZ0LSLaTWqtPxc8_5to_iA&usqp=CAU"
-              name="Virat Kohli"
-              lastMessage="You need to remove Dogecoin"
-              timestamp="Feb 9"
-            />
-            <RoomInfo
-              profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGD_UM4u8U6ksDgkoZ7Za_H8G38GjEuthdZQ&usqp=CAU"
-              name="MS Dhoni"
-              isLastMessageSentByMe
-              lastMessage="How about Ether?"
-              timestamp="Jan 31"
-            />
-            <RoomInfo
-              profilePic="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQ21dCAz7ZagwiwjBqCKcgyE1mQbrEwxYJw9Npwg5iRX0aok3UKaFNnA9nbJLAK"
-              name="Rahul Dravid"
-              lastMessage="Hi Aravindh! How are you?"
-              timestamp="Jan 21"
-            />
-            <RoomInfo
-              profilePic="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRTLO6mzbNcykX9Eh-lcYRhv7aaPmGfSha2ti2tp6rEa2hpPsC0"
-              name="Harry Styles"
-              isLastMessageSentByMe
-              lastMessage="You need to tweet more"
-              timestamp="Jan 1"
-            />
-          </div>
+      <div
+        style={!active ? { display: "none" } : { display: "block" }}
+        className="chat__body"
+      >
+        <div className="chat__search">
+          <Search />
+          <input type="text" placeholder="Search messages" />
+          <IconButton disableRipple>
+            <MenuIcon />
+          </IconButton>
         </div>
-      )}
+        <div className="chat_rooms">
+          <RoomInfo
+            profilePic="https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg"
+            name="Elon Musk"
+            isLastMessageSentByMe
+            lastMessage="Hi Musk! How are you?"
+            timestamp="8:11 PM"
+          />
+          <RoomInfo
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgvp8NhpQNyMKvqZ0LSLaTWqtPxc8_5to_iA&usqp=CAU"
+            name="Virat Kohli"
+            lastMessage="You need to remove Dogecoin"
+            timestamp="Feb 9"
+          />
+          <RoomInfo
+            profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGD_UM4u8U6ksDgkoZ7Za_H8G38GjEuthdZQ&usqp=CAU"
+            name="MS Dhoni"
+            isLastMessageSentByMe
+            lastMessage="How about Ether?"
+            timestamp="Jan 31"
+          />
+          <RoomInfo
+            profilePic="http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcQ21dCAz7ZagwiwjBqCKcgyE1mQbrEwxYJw9Npwg5iRX0aok3UKaFNnA9nbJLAK"
+            name="Rahul Dravid"
+            lastMessage="Hi Aravindh! How are you?"
+            timestamp="Jan 21"
+          />
+          <RoomInfo
+            profilePic="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRTLO6mzbNcykX9Eh-lcYRhv7aaPmGfSha2ti2tp6rEa2hpPsC0"
+            name="Harry Styles"
+            isLastMessageSentByMe
+            lastMessage="You need to tweet more"
+            timestamp="Jan 1"
+          />
+        </div>
+      </div>
     </div>
   );
 }
